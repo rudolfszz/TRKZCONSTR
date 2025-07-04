c91830a6fe4e212c66d2614b4625f6851e485c42
Rudolfs Zihars <R.Zihars@student.tudelft.nl>
2025-07-04 11:40:34 +0300

    Renamed to main

--- main/services/googleService.js
+++ main/services/googleService.js
@@ -7,7 +7,10 @@ export const SCOPES = [
     'https://www.googleapis.com/auth/drive.file',
     'https://www.googleapis.com/auth/spreadsheets',
     'https://www.googleapis.com/auth/drive.readonly',
-    'https://www.googleapis.com/auth/drive.metadata.readonly'
+    'https://www.googleapis.com/auth/drive.metadata.readonly',
+    'https://www.googleapis.com/auth/documents',
+    'https://www.googleapis.com/auth/presentations',
+    'https://www.googleapis.com/auth/spreadsheets'
 ];
 
 export const getDriveClient = (req) => {
